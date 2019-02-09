99adc59b7ffa748be7c2d81e46baed1981693d6d
Jad El Ramadi <jadelramadi@gmail.com>
2019-02-09 01:41:42 -0500

    finalized the game

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,10 +1,10 @@
-// 76 words
-var wordList = ["chrome", "firefox", "codepen", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "banner", "browser", "screen", "mobile", "footer", "header", "typography", "responsive", "programmer", "css", "border", "compass", "grunt", "pixel", "document", "object", "ruby", "modernizr", "bootstrap", "python", "php", "pattern", "ajax", "node", "element", "android", "application", "adobe", "apple", "google", "microsoft", "bookmark", "internet", "icon", "svg", "background", "property", "syntax", "flash", "html", "font", "blog", "network", "server", "content", "database", "socket", "function", "variable", "link", "apache", "query", "proxy", "backbone", "angular", "email", "underscore", "cloud"];
+// 20 words
+var wordList = ["chrome", "firefox", "codepen", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "python"];
 var guessedLTR = [];
 var correctGuess = [];
 var correctWord;
 var wins = 0;
-var trys;
+var tries = 0;
 
 function randomize() {
     var blankWord = [];
@@ -17,52 +17,69 @@ function randomize() {
         blank.textContent = "_";
         wordBlankDiv.appendChild(blank);
     }
-    console.log("Correct word: " + correctWord);
+    tries = Math.floor(wordList[random].length * 1.5);
+    var triesDiv = document.getElementById("tries");
+    triesDiv.innerHTML = tries;
+    var imgPath = "assests/images/" + wordList[random] + ".png";
+    var img = document.getElementById("imgHint");
+    img.setAttribute("src", imgPath);
+    img.setAttribute("alt", wordList[random]);
 }
 
 function validation(letter) {
     var previousIndex = -1;
     var foundIndex;
-    for (let i = 0; i < correctWord.length; i++) {
-        // previousIndex = i;
-        foundIndex = correctWord.indexOf(letter, previousIndex + 1);
-        if (foundIndex != -1) {
-            previousIndex = foundIndex;
-            console.log("FoundIndex: " + foundIndex);
-            let wordBlankDiv = document.getElementById("wordBlank");
-            wordBlankDiv.getElementsByTagName("span")[foundIndex].textContent = letter;
-            if (correctGuess.length != correctWord.length) {
+    if (!correctGuess.includes(letter)) {
+        for (let i = 0; i < correctWord.length; i++) {
+            foundIndex = correctWord.indexOf(letter, previousIndex + 1);
+            if (foundIndex != -1) {
+                previousIndex = foundIndex;
+                let wordBlankDiv = document.getElementById("wordBlank");
+                wordBlankDiv.getElementsByTagName("span")[foundIndex].textContent = letter;
                 correctGuess.push(letter);
-                console.log("Correct Guess: ", correctGuess);
-            } else {
-                previousIndex = 0;
-                wordBlankDiv.innerHTML = "";
-                randomize();
-                correctGuess = [];
-                wins++;
-                trys = 0;
-                guessedLTR = [];
-                console.log("Wins: ", wins)
-                break;
+                if (correctGuess.length < correctWord.length) {} else {
+                    previousIndex = 0;
+                    wordBlankDiv.innerHTML = "";
+                    randomize();
+                    correctGuess = [];
+                    var winsDiv = document.getElementById("wins");
+                    wins++;
+                    winsDiv.innerHTML = wins;
+                    guessedLTR = [];
+                    var guessedDiv = document.getElementById("guessed");
+                    guessedDiv.innerHTML = guessedLTR;
+                    break;
+                }
+
             }
-
         }
-    }
 
+    } else {
+        alert("You have already guessed: " + letter);
+    }
 }
 
 document.onkeypress = function(event) {
-    console.log("this is the guess: ", event.key);
     var guessedDiv = document.getElementById("guessed");
     if (correctWord.includes(event.key)) {
         validation(event.key);
     } else {
-        guessedLTR.push(event.key);
-        guessedDiv.innerHTML = guessedLTR;
+        var triesDiv = document.getElementById("tries");
+        if (tries <= 0) {
+            alert("You are out of tries. Better luck next time!");
+        } else {
+            if (!guessedLTR.includes(event.key)) {
+                guessedLTR.push(event.key);
+                guessedDiv.innerHTML = guessedLTR;
+                tries--;
+                triesDiv.innerHTML = tries;
+            } else {
+                alert("You have already guessed: " + event.key);
+            }
+        }
     }
 }
 
 window.onload = function(event) {
-    console.log(event);
     randomize();
 }
